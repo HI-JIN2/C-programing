c29ff049539234cdd5702afa05037b7a16f48480
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-02-04 12:54:04 +0900

    [ADD] Part 1- 도전문제 5번

--- KJE/challenge/1-5.c
+++ KJE/challenge/1-5.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
 int main(void){
     // 10개의 소수 출력
-    int num;
-    for(int i=2; i<30; i++){
+    // 입력받은 숫자보다 작은 모든 숫자들로 다 나눠 본다.
+    int ans=2, j=2;
+    for(int count=1; count<10; count++){
+        while(j<ans){
+            if(ans%j==0){
+                break;
+            }
+            else{
+                printf("소수 10개:%d\n",ans);
+                ans++;
+                j=2;
+                count++;
+            }
+            j++;
+        }
     }
     return 0;
 }

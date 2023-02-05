60d2f875dcdce1aab93de70754a04edcb34b9c7f
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-02-05 13:33:01 +0900

    [MOD] 도전문제 5번 코드 수정

--- KJE/challenge/1-5.c
+++ KJE/challenge/1-5.c
@@ -2,18 +2,17 @@
 int main(void){
     // 10개의 소수 출력
     // 입력받은 숫자보다 작은 모든 숫자들로 다 나눠 본다.
-    int ans=2;
-    for(int count=1; count<10; count++){  //10개 세기
-        for(int ans=2;ans<count;ans++){
-            for(int j=2; j<=ans;j++){
-                if(ans%j==0){
-                    break;
-                }
-                if(ans==j){
-                    printf("%d\n",ans);
-                }   
-            }
-        }
-    }
-    return 0;
+    int cnt ,num=30;
+	for (int i = 2; i <= num; i++)
+	{
+		cnt = 0;
+		for (int j = 2; j <= i; j++)
+		{
+			if (i % j == 0)
+				cnt++;
+		}
+		if (cnt == 1) // 나눠지는 수가 1개 뿐인 경우
+			printf("%d ", i); 
+
+	}
 }

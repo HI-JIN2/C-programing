d65531e138e15aab64482fea050f09d27b44297b
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-02-11 00:17:05 +0900

    [Modify] 2-3.c

--- KJE/challenge/2주차/2-3.c
+++ KJE/challenge/2주차/2-3.c
@@ -1,21 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(void){
     // 홀수이면 배열의 앞에서부터, 짝수면 뒤에서부터 채우기
-    int arr[10],n, min=0, max=9;
+    int arr[10], min=0, max=9;
     printf("총 10개의 숫자 입력\n");
     for(int i=0;i<10;i++){
         printf("입력:");
-        scanf("%d",&n);  // 왜 n으로 받아야 하는가? 배열로 받으면 안되는 건가?
-        if(n%2==1){
-            arr[min]= n;
+        scanf("%d",&arr[i]);
+
+        if(arr[i]%2==1){
+            int temp=0;
+            temp=arr[min];
+            arr[min]= arr[i];
             min++;
         }
         else{
-            arr[max]=n;
+            int temp=0;
+            temp=arr[max];
+            arr[max]= arr[i];
             max--;
         }
     }
-    printf("배열 요소의 출력:");
+    printf("배열 요소의 출력: ");
     for(int i=0;i<10;i++){
         printf("%d ",arr[i]);
     }

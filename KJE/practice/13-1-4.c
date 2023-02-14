3afedeccc34753fef34cb6042f256253903f86f1
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2023-02-14 19:37:16 +0900

    [Modify] 13-1-4.c

--- KJE/practice/13-1-4.c
+++ KJE/practice/13-1-4.c
@@ -12,8 +12,8 @@ int main(void){
         temp=*front;
         *front=*back;
         *back=temp;
-        front+=1;
-        back-=1;
+        front+=1;  //front 자체는 주소를 가르키고 *front는 해당 주소에 들어가 있는 값을 가르킨다. 
+        back-=1;  //front와 back을 동시에 각각 하나씩 증가하고 감소시키고 서로 값들을 바꾸는 식. 
         // 두개의 포인터 변수를 활용 해야 하므로 반복문은 3바퀴를 돌아 반반씩 하게끔!
     }
     for(int i=0;i<6;i++){

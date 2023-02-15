c477e67c3a28820ecbbfee6064edfb71cfeaae43
Yujin <jini1514@soongsil.ac.kr>
2023-02-15 17:58:55 +0900

    Modify 14-1-2.c

--- YJ/practice/14-1-2.c
+++ YJ/practice/14-1-2.c
@@ -1,15 +1,16 @@
-#include<stdio.h>
-void Swap3(int *ptr1, int *ptr2, int *ptr3){
+#include <stdio.h>
+void Swap3(int *ptr1, int *ptr2, int *ptr3)
+{
     int temp = *ptr3;
-    *ptr3=*ptr2;
-    *ptr2=*ptr1;
-    *ptr1=temp;
+    *ptr3 = *ptr2;
+    *ptr2 = *ptr1;
+    *ptr1 = temp;
 }
 
-int main(){
+int main()
+{
 
-    int num1=30, num2=40, num3=50;    
+    int num1 = 30, num2 = 40, num3 = 50;
     Swap3(&num1, &num2, &num3);
-    printf("%d %d %d",num1,num2,num3);
-
+    printf("%d %d %d", num1, num2, num3);
 }
